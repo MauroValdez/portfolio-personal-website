db511c55da421f07b0e3f8024cbc1f40fea10484
Mauro Valdez <josenova0@gmail.com>
2022-11-11 17:04:30 -0300

    footer scrollup darkMode

--- source/js/app.js
+++ source/js/app.js
@@ -1,9 +1,14 @@
 import Swiper from 'swiper/bundle';
 import emailjs from "@emailjs/browser"
 
+window.addEventListener('scroll', () => {
+  scrollActive()
+  showScrollup()
+})
 document.addEventListener('DOMContentLoaded', () => {
   showMenu()
   formEmail()
+  darkMode()
 })
 
 function showMenu() {
@@ -94,4 +99,53 @@ function formEmail() {
   }
 
   contactForm.addEventListener('submit', sendEmail)
+}
+
+function scrollActive() {
+  const sections = document.querySelectorAll('section[id]'),
+        scrollY = window.pageYOffset
+
+  sections.forEach( current => {
+    const sectionHeight = current.offsetHeight,
+          sectionTop = current.offsetTop - 58,
+          sectionId = current.getAttribute('id'),
+          sectionsClass = document.querySelector(`.nav__menu a[href*=${sectionId}]`)
+    
+    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add('active-link')
+    } else {
+      sectionsClass.classList.remove('active-link')
+    }
+  })
+}
+
+function showScrollup() {
+  const scrollUp = document.querySelector('#scroll-up')
+
+  window.scrollY >= 350 ? scrollUp.classList.add('show-scroll') : scrollUp.classList.remove('show-scroll')
+}
+
+function darkMode() {
+  const themeButton = document.querySelector('#theme-button'),
+        darkTheme = 'dark-theme',
+        iconTheme = 'ri-sun-line'  
+
+  const selectedTheme = localStorage.getItem('selected-theme'),
+        selectedIcon = localStorage.getItem('selected-icon')
+
+  const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+  const getCurrentIcon = () => document.body.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
+
+  if(selectedTheme) {
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
+  }
+
+  themeButton.addEventListener('click', () => {
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+  })
 }
